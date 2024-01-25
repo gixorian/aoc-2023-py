971bc6c7d61388c8174a2b278263c850be3a0bae
gixorian <goranborovecki1@gmail.com>
2024-01-25 23:11:26 +0100

    day-05 done(4.3 ms)

--- day-05/app.py
+++ day-05/app.py
@@ -1,5 +1,4 @@
-import os
-import concurrent.futures
+import time
 
 ######################################################
 
@@ -9,7 +8,8 @@ inputFile.close()
 
 ######################################################
 
-seeds = []
+p1Seeds = []
+p2Seeds = []
 index = 0
 maps = [[],[],[],[],[],[],[]]
 
@@ -17,7 +17,9 @@ for line in lines:
     line = line.strip("\n")
     if "seeds:" in line:
         line = line.strip("seeds: ")
-        seeds = line.split(" ")
+        p1Seeds = line.split(" ")
+        for i in range(0, len(p1Seeds), 2):
+            p2Seeds.append((int(p1Seeds[i]), int(p1Seeds[i]) + int(p1Seeds[i+1]) - 1, 0))
     elif "seed-to-soil map:" in line:
         index = 0
     elif "soil-to-fertilizer map:" in line:
@@ -39,10 +41,13 @@ for line in lines:
 
 
 def GetPart1Answer():
+    start_time = time.time()
+
     mappingNumber = 0
     rangeOffset = 0
     locations = []
-    for seed in seeds:
+    
+    for seed in p1Seeds:
         mappingNumber = int(seed)
         for map in maps:
             for mapRanges in map:
@@ -51,153 +56,59 @@ def GetPart1Answer():
                     mappingNumber = int(mapRanges[0]) + rangeOffset
                     break
         locations.append(mappingNumber)    
+    
+    print(str((time.time() - start_time) * 1000))
     return min(locations)
 
 
-### OPTIMIZATION:
-### Go in reverse
-### Go trough the locations first in order of lowest to highest
-### That way, unless the smallest possible location is near the end, it should be much faster
-
-### Even better idea (not sure if possible though)
-### Go trough all of the ranges first, and once we find which range contains the lowest location or reverse, depending on what's better and possible...
-### Than we just need to go trough all seeds or locations in that range
-
-### The fastest would probably be to combine the ideas
-### Go trough all of the location ranges first, starting with the lowest range
-### As soon as we find a sees/seed range that fits the location range, we go trough all of the locations in that range, stasrting with the lowest one again
-### This way we should be able to find the range without needing to go trough all of them, and then find the seed without needing to go trough all of them as well
-
-
 def GetPart2Answer():
-    mappingNumber = 0
-    rangeOffset = 0
-    locations = []
-    for seedIndex in range(0, len(seeds), 2):
-        if seedIndex < len(seeds)-1:
-            seedMax = int(seeds[seedIndex])+int(seeds[seedIndex+1])
-        else:
-            seedMax = int(seeds[seedIndex])+1
-            
-        for seedNum in range(int(seeds[seedIndex]), seedMax): 
-            mappingNumber = seedNum
-            if mappingNumber % 1000 == 0:
-                os.system('clear')
-            for map in maps:
-                for mapRanges in map:
-                    if mappingNumber >= int(mapRanges[1]) and int(mappingNumber) < int(mapRanges[1])+int(mapRanges[2]):
-                        rangeOffset = mappingNumber - int(mapRanges[1])
-                        mappingNumber = int(mapRanges[0]) + rangeOffset
-                        break
-            locations.append(mappingNumber)    
-        return min(locations) 
-
-
-def locationMap(mappingNumber):
-    location = mappingNumber
-    if mappingNumber % 10000 == 0:
-        os.system('clear')
-        print(location)
-    for map in range(len(maps)-1, -1, -1):
-        for mapRange in maps[map]:
-            rangeStart = int(mapRange[0])
-            rangeEnd = int(mapRange[0]) + int(mapRange[2])
-            if mappingNumber >= rangeStart and mappingNumber < rangeEnd:
-                rangeOffset = mappingNumber - int(mapRange[0])
-                mappingNumber = int(mapRange[1]) + rangeOffset
-                break
-
-    for i in range(0, len(seeds), 2):
-        if mappingNumber >= int(seeds[i]) and mappingNumber < int(seeds[i]) + int(seeds[i+1]):
-            return location
-
-def GetParallelPart2Answer():
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(locationMap, mappingNumber) for mappingNumber in range(20000000, 1000000000)]
-
-        results = [future.result() for future in concurrent.futures.as_completed(futures)]
-        return results
-
-
-def GetReversePart2Answer():
-    rangeOffset = 0
-    for mappingNumber in range(1000000, 1000000000):
-        location = mappingNumber
-        if mappingNumber % 1000 == 0:
-            os.system('clear')
-            print(location)
-        for map in range(len(maps)-1, -1, -1):
-            for mapRange in maps[map]:
-                rangeStart = int(mapRange[0])
-                rangeEnd = int(mapRange[0]) + int(mapRange[2])
-                if mappingNumber >= rangeStart and mappingNumber < rangeEnd:
-                    rangeOffset = mappingNumber - int(mapRange[0])
-                    mappingNumber = int(mapRange[1]) + rangeOffset
-                    break
-
-        for i in range(0, len(seeds), 2):
-            if mappingNumber >= int(seeds[i]) and mappingNumber < int(seeds[i]) + int(seeds[i+1]):
-                return location
-
-
-def GetOptimizedPart2Answer():
-    mappingNumber = 0
-    rangeOffset = 0
-    locations = []
-    remaining = []
-    for i in range(0, len(seeds), 2):
-        seedStart = int(seeds[i])
-        seedEnd = int(seeds[i]) + int(seeds[i+1]) - 1
+    start_time = time.time()
+    lowestLocation = float("inf")
+    
+    while p2Seeds:
+        
+        seedStart, seedEnd, seedDepth = p2Seeds.pop(0)
+        depth = 0
+        canContinue = False
 
         for map in maps:
-            for mapRange in map:
-                sourceRangeStart = int(mapRange[1])
-                sourceRangeEnd = int(mapRange[1]) + int(mapRange[2])
-                targetRangeStart = int(mapRange[0])
-                targetRangeEnd = int(mapRange[0]) + int(mapRange[2])
-                print(i)
-                #print("seedEnd: {}, targetRangeStart: {} + ( seedEnd: {} - sourceRangeStart: {}, )".format(seedEnd, targetRangeStart, seedEnd, sourceRangeStart))
-                if seedStart >= sourceRangeStart and seedEnd < sourceRangeEnd:
-                    #The whole range is in the mapped range
-                    print("source start: {}, source end: {}".format(seedStart,seedEnd))
-                    seedStart = targetRangeStart + (seedStart - sourceRangeStart)
-                    seedEnd = targetRangeStart + (seedEnd - sourceRangeStart)
-                    print("target start: {}, target end: {}".format(seedStart,seedEnd))  
-                    break
-                elif seedStart >= sourceRangeStart and seedStart < sourceRangeEnd and seedEnd >= sourceRangeEnd:
-                    #The whole range is not in the mapped range, need to split it, extra is on the right 
-                    print("source start: {}, source end: {}".format(seedStart,seedEnd))
-                    remaining.append((sourceRangeEnd, seedEnd))
-                    seedStart = targetRangeStart + (seedStart - sourceRangeStart)
-                    seedEnd = targetRangeStart + (sourceRangeEnd-1 - sourceRangeStart)
-                    print("target start: {}, target end: {}".format(seedStart,seedEnd))  
-                    print("remaining start: {}, remaining end: {}\n".format(remaining[len(remaining)-1][0], remaining[len(remaining)-1][1]))  
-                    break
-                elif seedStart < sourceRangeStart and seedEnd > sourceRangeStart and seedEnd <= sourceRangeEnd:
-                    #The whole range is not in the mapped range, need to split it, extra is on the left 
-                    print("source start: {}, source end: {}".format(seedStart,seedEnd))
-                    remaining.append((seedStart, sourceRangeStart-1))
-                    seedStart = targetRangeStart + (sourceRangeStart - sourceRangeStart)
-                    seedEnd = targetRangeStart + (seedEnd - sourceRangeStart)
-                    print("target start: {}, target end: {}".format(seedStart,seedEnd))  
-                    print("remaining start: {}, remaining end: {}\n".format(remaining[len(remaining)-1][0], remaining[len(remaining)-1][1]))  
-                    break
-                else:
-                    #The whole range is not in the mapped range, check the other ranges
-                    print("\n")
-                
-
-    #         for map in maps:
-    #             for mapRanges in map:
-    #                 if mappingNumber >= int(mapRanges[1]) and int(mappingNumber) < int(mapRanges[1])+int(mapRanges[2]):
-    #                     rangeOffset = mappingNumber - int(mapRanges[1])
-    #                     mappingNumber = int(mapRanges[0]) + rangeOffset
-    #                     break
-    #         locations.append(mappingNumber)    
-    #     return min(locations) 
     
- 
-GetOptimizedPart2Answer()
+            if seedDepth == depth or canContinue:
+                canContinue = True
+        
+                for mapRange in map:
+                    
+                    sourceRangeStart = int(mapRange[1])
+                    sourceRangeEnd = int(mapRange[1]) + int(mapRange[2])
+                    targetRangeStart = int(mapRange[0])
+                    
+                    if seedStart >= sourceRangeStart and seedEnd < sourceRangeEnd:
+                        #The whole range is in the mapped range
+                        seedStart = targetRangeStart + (seedStart - sourceRangeStart)
+                        seedEnd = targetRangeStart + (seedEnd - sourceRangeStart)
+                        break
+                    elif seedStart >= sourceRangeStart and seedStart < sourceRangeEnd and seedEnd >= sourceRangeEnd:
+                        #The whole range is not in the mapped range, need to split it, extra is on the right 
+                        p2Seeds.append((sourceRangeEnd, seedEnd, depth))
+                        seedStart = targetRangeStart + (seedStart - sourceRangeStart)
+                        seedEnd = targetRangeStart + (sourceRangeEnd-1 - sourceRangeStart)
+                        break
+                    elif seedStart < sourceRangeStart and seedEnd > sourceRangeStart and seedEnd <= sourceRangeEnd:
+                        #The whole range is not in the mapped range, need to split it, extra is on the left 
+                        p2Seeds.append((seedStart, sourceRangeStart-1, depth))
+                        seedStart = targetRangeStart + (sourceRangeStart - sourceRangeStart)
+                        seedEnd = targetRangeStart + (seedEnd - sourceRangeStart)
+                        break
+            
+            depth += 1
 
-#print("Part 1 Answer: " + str(GetPart1Answer()))
-#print("Part 2 Answer: " + str(GetOptimizedPart2Answer()))
+        if seedStart < lowestLocation:
+            lowestLocation = seedStart
+
+    print(str((time.time() - start_time) * 1000))
+    return lowestLocation
+
+
+print("Part 1 Answer: " + str(GetPart1Answer()))
+print("")
+print("Part 2 Answer: " + str(GetPart2Answer()))
